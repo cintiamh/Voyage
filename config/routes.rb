d50816058ad558154117b2c24ead2b27b34f1e3d
Cintia Higashi <cintiamh@gmail.com>
2013-04-02 00:30:10 -0400

    Created admin dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -97,6 +97,10 @@ Voyage::Application.routes.draw do
   #     resources :products
   #   end
 
+  namespace :admin do
+    get "dashboard/index"
+  end
+
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => 'pages#index'
