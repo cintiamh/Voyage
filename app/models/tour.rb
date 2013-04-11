ec81ef8e0ab2e25e24a67671ee5e54aaf3a67486
Cintia Higashi <cintiamh@gmail.com>
2013-04-11 10:55:39 -0400

    fix for tour model to accept pieces and informations

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -9,7 +9,7 @@ class Tour < ActiveRecord::Base
   has_and_belongs_to_many :pieces
   has_and_belongs_to_many :categories
 
-  attr_accessible :about, :curated, :image, :public, :title, :user_id, :tour_histories, :informations, :comments, :likes, :pieces, :categories, :image_file_name, :image_content_type, :image_file_size, :image_updated_at
+  attr_accessible :about, :curated, :image, :public, :title, :user_id, :tour_histories, :comments, :likes, :categories, :image_file_name, :image_content_type, :image_file_size, :image_updated_at, :pieces_attributes, :informations_attributes
 
   has_attached_file :image
 end
