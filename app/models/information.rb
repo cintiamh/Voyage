8a122f3de0edf12ac4d58edc32840f8348124fa2
Cintia Higashi <cintiamh@gmail.com>
2013-04-12 11:16:00 -0400

    Basic tour forms working

--- app/models/information.rb
+++ app/models/information.rb
@@ -2,7 +2,7 @@ class Information < ActiveRecord::Base
   belongs_to :piece
   belongs_to :connection
   belongs_to :tour
-  attr_accessible :after, :before, :piece_id
+  attr_accessible :after, :before, :piece_id, :tour_id
 
   validates :before, :presence => true
   validates :after, :presence => true
