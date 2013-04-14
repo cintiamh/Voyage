d1fa228f56002a8ab6a77528b5f6f0ef171797bd
Cintia Higashi <cintiamh@gmail.com>
2013-04-14 15:24:59 -0400

    included correct book value to answer

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130414180243) do
+ActiveRecord::Schema.define(:version => 20130414191504) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(:version => 20130414180243) do
     t.string   "content"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.boolean  "correct"
   end
 
   add_index "answers", ["question_id"], :name => "index_answers_on_question_id"
