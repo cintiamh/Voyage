3e96c6b29c0538855c79d3dfbd561a67da9eacdf
Cintia Higashi <cintiamh@gmail.com>
2013-04-17 16:20:37 -0400

    Included NH fields to pieces form, Uniformized styles for listings

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -15,7 +15,7 @@ class Piece < ActiveRecord::Base
   scope :inExhibition, where(:exhibited => true)
   scope :notExhibited, where(:exhibited => false)
 
-  attr_accessible :accession_number, :credit, :exhibited, :measurements, :medium, :title, :image, :image_content_type, :image_file_name, :image_file_size, :image_updated_at, :questions_attributes, :informations_attributes, :creator_id, :gallery_id, :location, :connections_attributes, :tour_attributes
+  attr_accessible :accession_number, :credit, :exhibited, :measurements, :medium, :title, :image, :image_content_type, :image_file_name, :image_file_size, :image_updated_at, :questions_attributes, :informations_attributes, :creator_id, :gallery_id, :location, :connections_attributes, :tour_attributes, :museum, :latin, :period, :collection_number, :description
 
   validates :title, :presence => true
 
