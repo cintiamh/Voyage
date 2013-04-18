c2856fd9c282361582f5c75d9a1fdf59706d4841
Cintia Higashi <cintiamh@gmail.com>
2013-04-18 15:55:50 -0400

    changed model access for user, comments, likes

--- app/models/state.rb
+++
@@ -1,9 +0,0 @@
-class State < ActiveRecord::Base
-  belongs_to :country
-  has_many :cities
-  has_many :pieces
-
-  attr_accessible :name, :country_id, :cities_attributes
-
-  accepts_nested_attributes_for :cities, allow_destroy: true
-end
