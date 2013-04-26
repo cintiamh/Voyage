8a19a6487aee1c5f84840bd29fd2412f8cbf16ed
Cintia Higashi <cintiamh@gmail.com>
2013-04-26 15:14:51 -0400

    adding js and css files to precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,7 @@ Voyage::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w[admin.js desktop.js identities_view_logic.js jquery.rwdImageMaps.js login.js mobile.js paloma/* active_admin.js admin.css]
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
