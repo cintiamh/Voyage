5fd9110603976021e635946af6d4223147df775a
Cintia Higashi <cintiamh@gmail.com>
2013-04-26 14:49:42 -0400

    fix s3 bucket path

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,6 +44,7 @@ Voyage::Application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
+  config.action_controller.asset_host = "https://s3.amazonaws.com/#{ENV['AWS_BUCKET']}"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
