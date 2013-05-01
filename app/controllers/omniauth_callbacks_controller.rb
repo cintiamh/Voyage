1a8d7b1b57149b2a8c4ed3a2be68a14a9c71ffd7
Cintia Higashi <cintiamh@gmail.com>
2013-05-01 18:42:00 -0400

    changed sign up/sign in forms styles

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,6 +1,6 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
-  layout "mobile"
+  layout "admin"
 
   def all
     user = User.from_omniauth(request.env["omniauth.auth"])
